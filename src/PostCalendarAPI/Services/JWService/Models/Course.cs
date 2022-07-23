a7b6a2c76100605c698dc8338855f5d97942fe25
jackfiled <xcrenchangjun@outlook.com>
2022-07-23 16:58:46 +0800

    解析上课周数的函数完成 添加了相关的单元测试

--- src/PostCalendarAPI/Services/JWService/Models/Course.cs
+++ src/PostCalendarAPI/Services/JWService/Models/Course.cs
@@ -1,39 +1,118 @@
-﻿namespace PostCalendarAPI.Services.JWService.Models
-{
-#nullable disable
+﻿using System.Text.RegularExpressions;
 
+namespace PostCalendarAPI.Services.JWService.Models
+{
     /// <summary>
     ///  表示学期中的每一个课的模型
     /// </summary>
-    internal class Course
+    public class Course
     {
         /// <summary>
         /// 课程的名称
         /// </summary>
-        public string Name { get; set; }
+        public string Name { get; }
         /// <summary>
         /// 任课教师
         /// </summary>
-        public string Teacher { get; set; }
+        public string Teacher { get; }
         /// <summary>
         /// 上课的地点
         /// </summary>
-        public string Place { get; set; }
+        public string Place { get; }
         /// <summary>
         /// 上课的周数
         /// </summary>
-        public int[] Weeks { get; set; }
+        public int[] Weeks { get; }
         /// <summary>
         /// 课程的开始时间
         /// </summary>
-        public TimeOnly BeginTime { get; set; }
+        public TimeOnly BeginTime { get; }
         /// <summary>
         /// 课程的结束时间
         /// </summary>
-        public TimeOnly EndTime { get; set; }
+        public TimeOnly EndTime { get; }
         /// <summary>
         /// 课程所在的星期
         /// </summary>
-        public DayOfWeek DayOfWeek { get; set; }
+        public int DayOfWeek { get; }
+
+        /// <summary>
+        /// 创建课程对象
+        /// </summary>
+        /// <param name="name">课程的名称</param>
+        /// <param name="teacher">任课教师</param>
+        /// <param name="place">上课地点</param>
+        /// <param name="weeks">上课的周数</param>
+        /// <param name="beginClass">开始的节次</param>
+        /// <param name="endClass">结束的节次</param>
+        /// <param name="dayOfWeek">星期几</param>
+
+        public Course(
+            string name,
+            string teacher,
+            string place,
+            int[] weeks,
+            int beginClass,
+            int endClass,
+            int dayOfWeek
+            )
+        {
+            Name = name;
+            Teacher = teacher;
+            Place = place;
+
+            Weeks = weeks;
+
+            BeginTime = BeginTimeList[beginClass - 1];
+            EndTime = BeginTimeList[endClass - 1].AddMinutes(ClassLength);
+
+            DayOfWeek = dayOfWeek;
+        }
+
+        public static int[] AnalyseWeekString(string weeks)
+        {
+            Regex pattern = new Regex(@"^(\d+)-(\d+).*");
+            List<int> ints = new List<int>();
+
+            var lines = weeks.Split(",");
+            for(int i = 0; i < lines.Length; i++)
+            {
+                var line = lines[i];
+                Match m = pattern.Match(line);
+                if(m.Success)
+                {
+                    int begin = int.Parse(m.Groups[1].Value);
+                    int end = int.Parse(m.Groups[2].Value);
+
+                    for(int j = begin; j <= end; j++)
+                    {
+                        ints.Add(j);
+                    }
+                }
+            }
+
+            return ints.ToArray();
+        }
+
+
+        private static TimeOnly[] BeginTimeList = new TimeOnly[]
+        {
+            new TimeOnly(8, 0),
+            new TimeOnly(8, 50),
+            new TimeOnly(9, 50),
+            new TimeOnly(10, 40),
+            new TimeOnly(11, 30),
+            new TimeOnly(13, 0),
+            new TimeOnly(13, 50),
+            new TimeOnly(14, 45),
+            new TimeOnly(15, 40),
+            new TimeOnly(16, 35),
+            new TimeOnly(17, 25),
+            new TimeOnly(18, 35),
+            new TimeOnly(19, 20),
+            new TimeOnly(20, 10),
+        };
+
+        private static readonly int ClassLength = 45;
     }
 }
---
+++ src/PostCalendarAPITests/ExcelAnalysisTest.cs
@@ -0,0 +1,46 @@
+﻿using PostCalendarAPI.Services.JWService.Models;
+
+
+namespace PostCalendarAPITests
+{
+    [TestClass]
+    public class ExcelAnalysisTest
+    {
+        /// <summary>
+        /// 测试识别上课的周数
+        /// </summary>
+        [TestMethod]
+        public void AnalyseWeekStringTest1()
+        {
+            string input = "1-16[周]";
+            var result = Course.AnalyseWeekString(input);
+
+            for(int i = 1; i <= 16; i++)
+            {
+                Assert.IsTrue(result.Contains(i));
+            }
+        }
+
+        /// <summary>
+        /// 测试识别上课的周数
+        /// </summary>
+        [TestMethod]
+        public void AnalyseWeekStringTest2()
+        {
+            string input = "1-10,14-16[周]";
+            var result = Course.AnalyseWeekString(input);
+
+            for(int i = 1; i <= 10; i++)
+            {
+                Assert.IsTrue(result.Contains(i));
+            }
+
+            for(int i = 14; i <= 16; i++)
+            {
+                Assert.IsTrue(result.Contains(i));
+            }
+        }
+
+
+    }
+}
