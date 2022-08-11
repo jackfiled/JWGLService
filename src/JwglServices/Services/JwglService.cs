eb8a43c455d3adf2c149e59990b548779034bca8
jackfiled <xcrenchangjun@outlook.com>
2022-08-11 18:00:37 +0800

    添加gRPC微服务项目

---
+++ src/JwglServices/Services/JwglService.cs
@@ -0,0 +1,12 @@
+﻿using Grpc.Core;
+
+namespace JwglServices.Services
+{
+    public class JwglService : Jwgler.JwglerBase
+    {
+        public override Task<GetSemesterResponse> GetSemester(GetSemesterRequest request, ServerCallContext context)
+        {
+            return base.GetSemester(request, context);
+        }
+    }
+}
