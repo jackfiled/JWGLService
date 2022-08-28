71b2a39325f8f32b9cc8d461e2411044bad03cad
jackfiled <xcrenchangjun@outlook.com>
2022-08-28 11:41:38 +0800

    添加了初始化数据库的sql文件

---
+++ src/JwglBackend/semesters.sql
@@ -0,0 +1,3 @@
+﻿insert into Semesters values (null, "2021-2022-1", "2021-08-30T00:00:00");
+insert into Semesters values (null, "2021-2022-2", "2022-02-28T00:00:00");
+insert into Semesters values (null, "2022-2023-1", "2022-08-22T00:00:00");
